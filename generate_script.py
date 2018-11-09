566f310c273b35cbfcbe69669438cddcf646ed78
Sativus <35407551+ehugos@users.noreply.github.com>
2018-11-09 14:16:46 +0100

    Add files via upload

--- generate_script.py
+++ generate_script.py
@@ -3,20 +3,22 @@
 import sys
 from Bio import SeqIO
 
-# Script for removing all taxa defined in remove_list
+# Script for removing all taxa defined in a previosuly remove_list
+
 def filter_taxa():
-	# Defines in and outfiles as well as the remove_id set
+	# Defines in and outfiles as well as the remove_id set using user input
 	infile = sys.argv[1]
 	remove_list = sys.argv[2]
 	outfile = sys.argv[3]
 	remove_id =  set([])
 	
-	# Opens and reads the file containing taxa to remove
+	# Opens and reads the remove-list file containing taxa to remove
 	with open (remove_list, 'r') as remove_f:
 		for line in remove_f:
 			line = line.strip()
-			# Removes the > as to match the taxa.id 
+			# Removes the > as to match the taxa.id given from SeqIO
 			remove_id.add(str(line).replace(">", ""))	
+	# Opens the in-file, reads the lines
 	with open(infile) as ori_fasta, open(outfile, 'w') as edited:
 		taxas = SeqIO.parse(ori_fasta, 'fasta')
 		# Checks all taxa if they match the remove_id set, if not, they are printed out
